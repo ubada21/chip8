81e22dd059f4d42331f100dde293714ca3bf15bd
ubada <ubada_21@hotmail.com>
2024-02-27 13:46:32 -0800

    init emu

---
+++ emu/target/debug/deps/chip8_core-5ecaccc39443b035.d
@@ -0,0 +1,7 @@
+/Users/ubada/chip8/emu/target/debug/deps/libchip8_core-5ecaccc39443b035.rmeta: /Users/ubada/chip8/chip8_core/src/lib.rs
+
+/Users/ubada/chip8/emu/target/debug/deps/libchip8_core-5ecaccc39443b035.rlib: /Users/ubada/chip8/chip8_core/src/lib.rs
+
+/Users/ubada/chip8/emu/target/debug/deps/chip8_core-5ecaccc39443b035.d: /Users/ubada/chip8/chip8_core/src/lib.rs
+
+/Users/ubada/chip8/chip8_core/src/lib.rs:
---
+++ emu/target/debug/deps/emu-bce780c27b2583fc.d
@@ -0,0 +1,5 @@
+/Users/ubada/chip8/emu/target/debug/deps/emu-bce780c27b2583fc: src/main.rs
+
+/Users/ubada/chip8/emu/target/debug/deps/emu-bce780c27b2583fc.d: src/main.rs
+
+src/main.rs:
